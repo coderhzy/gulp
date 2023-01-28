39960a521209fc7a941d4eba3d70fa1363b5ed03
codehzy <1257664828@qq.com>
2023-01-28 11:06:40 +0800

    feat(gulp打包项目): ✨ gulp打包项目

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,33 @@
 const { src, dest } = require("gulp");
 const htmlmin = require('gulp-htmlmin')
+const babel = require('gulp-babel')
+const terser = require('gulp-terser')
+const less = require('gulp-less')
+const inject = require('gulp-inject')
 
+// 1. 对html进行打包
 const htmlTask = () => {
   return src("./src/**/*.html").pipe(htmlmin({ collapseWhitespace: true })).pipe(dest("./dist"));
 };
 
+// 2, 对js进行打包
+const jsTask = () => {
+  return src('./src/**/*.js').pipe(babel()).pipe(terser()).pipe(dest('./dist'))
+};
+
+// 3. 对less打包
+const lessTask = () => {
+  return src('./src/**/*.less').pipe(less()).pipe(dest('./dist'))
+}
+
+// 4. 对html中的js和css进行注入
+const injectTask = () => {
+  return src('./dist/**/*.html').pipe(inject(src(['./dist/**/*.js', './dist/**/*.css']))).pipe(dest('./dist'))
+}
+
 module.exports = {
   htmlTask,
+  jsTask,
+  lessTask,
+  injectTask
 };
